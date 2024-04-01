bb5bea0f56f35ec0b01da7648aede91373e5f0f9
SquirrelKiev <laidbackenon@outlook.com>
2024-04-01 08:08:12 +0100

    migrations

---
+++ MemBotReal/Migrations/PostgresMigrations/20240401070702_InitialCreate.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace MemBotReal.Migrations.PostgresMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CaseType = table.Column<int>(type: "integer", nullable: false),
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    OffenderId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    ModId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Reason = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomCommands",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    OwnerId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Contents = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomCommands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DelayedUnmutes",
+                columns: table => new
+                {
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    OffenderId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    UnmuteWhen = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DelayedUnmutes", x => new { x.GuildId, x.OffenderId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuildConfigs",
+                columns: table => new
+                {
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    MuteRole = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    ModRole = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    LoggingChannel = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    CasesAllowedChannels = table.Column<decimal[]>(type: "numeric(20,0)[]", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildConfigs", x => x.GuildId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuildPrefixPreferences",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    GuildId = table.Column<decimal>(type: "numeric(20,0)", nullable: false),
+                    Prefix = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildPrefixPreferences", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cases_GuildId_OffenderId",
+                table: "Cases",
+                columns: new[] { "GuildId", "OffenderId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomCommands_GuildId_Name",
+                table: "CustomCommands",
+                columns: new[] { "GuildId", "Name" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "CustomCommands");
+
+            migrationBuilder.DropTable(
+                name: "DelayedUnmutes");
+
+            migrationBuilder.DropTable(
+                name: "GuildConfigs");
+
+            migrationBuilder.DropTable(
+                name: "GuildPrefixPreferences");
+        }
+    }
+}
---
+++ MemBotReal/Migrations/SqliteMigrations/20240401070641_InitialCreate.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MemBotReal.Migrations.SqliteMigrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Cases",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    CaseType = table.Column<int>(type: "INTEGER", nullable: false),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    OffenderId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ModId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Reason = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Cases", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CustomCommands",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    OwnerId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Contents = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomCommands", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DelayedUnmutes",
+                columns: table => new
+                {
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    OffenderId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    UnmuteWhen = table.Column<DateTimeOffset>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DelayedUnmutes", x => new { x.GuildId, x.OffenderId });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuildConfigs",
+                columns: table => new
+                {
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    MuteRole = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    ModRole = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    LoggingChannel = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    CasesAllowedChannels = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildConfigs", x => x.GuildId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "GuildPrefixPreferences",
+                columns: table => new
+                {
+                    Id = table.Column<uint>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    GuildId = table.Column<ulong>(type: "INTEGER", nullable: false),
+                    Prefix = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GuildPrefixPreferences", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Cases_GuildId_OffenderId",
+                table: "Cases",
+                columns: new[] { "GuildId", "OffenderId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_CustomCommands_GuildId_Name",
+                table: "CustomCommands",
+                columns: new[] { "GuildId", "Name" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Cases");
+
+            migrationBuilder.DropTable(
+                name: "CustomCommands");
+
+            migrationBuilder.DropTable(
+                name: "DelayedUnmutes");
+
+            migrationBuilder.DropTable(
+                name: "GuildConfigs");
+
+            migrationBuilder.DropTable(
+                name: "GuildPrefixPreferences");
+        }
+    }
+}
