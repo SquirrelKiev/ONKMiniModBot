1ffbf7cd136cbb0e3d82ebdd2dc1b9192e92ddad
SquirrelKiev <laidbackenon@outlook.com>
2024-04-01 17:31:45 +0100

    update help page

--- MemBotReal/Modules/Help/HelpService.cs
+++ MemBotReal/Modules/Help/HelpService.cs
@@ -9,32 +9,45 @@ public class HelpService
     public MessageContents GetMessageContents()
     {
         var embed = new EmbedBuilder()
-            .WithDescription("### ⭐ Mute / Unmute" +
-            "\n\n" +
-            "`Syntax: ,mute [user] [length] [reason]`\n" +
-            "Supply a **#d#h#m#s** for a timed mute. \n\n" +
-            "Examples:\n" +
-            "`,mute @user optional reason` - will mute the user indefinitely\n" +
-            "`,mute USER_ID` - can also use id instead of mention\n" +
-            "`,mute @user 2h30m Optional reason goes here`\n" +
-            "`,unmute @user` - will unmute the user\n" +
-            "`,unmute @user Optional reason` - will unmute the user with a reason\n\n" +
-            "### ⭐ Warn\n\n" +
-            "`Syntax: ,warn <user> <reason>`\n" +
-            "**Warn a user with a necessary supplied reason.**\n" +
-            "`,warn @user Reason goes here` (reason has to be supplied)\n" +
-            "`,warn user_id Reason goes here`\n\n" +
-            "### ⭐ Cases\n" +
-            "Check the past mutes and warns for a user\n" +
-            "`Syntax: ,cases <offender>`\n\n" +
-            "### ⭐Custom Commands\n\n" +
-            "`Syntax: ,add <name> <contents>`\n" +
-            "**Add a custom command**\n" +
-            ",add suisei suisei.png\n\n" +
-            "`Syntax: ,remove <name>`\n" +
-            "**Remove a custom command**\n\n" +
-            "**List Commands**\n" +
-            "`,listcmds` - Gives a list of all the custom commands.")
+            .WithDescription(
+@"### :star: Mute / Unmute
+
+`Syntax: ,mute [user] [length] [reason]`
+Supply a **#d#h#m#s** for a timed mute. 
+
+Examples:
+`,mute @user optional reason` - will mute the user indefinitely
+`,mute USER_ID` - can also use id instead of mention
+`,mute @user 2h30m Optional reason goes here`
+`,unmute @user` - will unmute the user
+`,unmute @user Optional reason` - will unmute the user with a reason
+
+### :star: Warn
+
+`Syntax: ,warn <user> <reason>`
+**Warn a user with a necessary supplied reason.**
+`,warn @user Reason goes here` (reason has to be supplied)
+`,warn user_id Reason goes here`
+
+### :star: Cases
+`Syntax: ,cases <offender>` - Check the past mutes and warns for a user
+`Syntax: ,casesby <mod>`- Check the past mutes and warns made by a mod
+
+### :star:Custom Commands
+
+`Syntax: ,add <name> <contents>`
+**Add a custom command**
+,add suisei suisei.png
+
+`Syntax: ,remove <name>`
+**Remove a custom command**
+
+**List Commands**
+`,listcmds` - Gives a list of all the custom commands.
+
+### :star:Nicknames
+
+`Syntax: ,nick <user> <nickname>` - Changes the nickname of that user. Nickname must be under 32 characters.")
             .WithColor(0xff3eb3);
 
         return new MessageContents(embed, new ComponentBuilder());
